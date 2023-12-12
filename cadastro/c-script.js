a9b5d148844fc822883acdbbc1b8c43f47ed7b28
Lauberr <barbaralauber3@gmail.com>
2023-12-12 19:17:59 -0300

    organizando arquivo c-style  (style do cadastro)

--- cadastro/c-script.js
+++ cadastro/c-script.js
@@ -5,9 +5,29 @@ let welcome = document.querySelector('.welcome')
 
 function menuRegister(){
     if (registerMenu.computedStyleMap.bottom == '-100%'  ||  registerMenu.computedStyleMap.bottom == ''){
-        registerMenu.computedStyleMap.bottom = "100%"  
-        
+        registerMenu.computedStyleMap.bottom = "100%"     
     }
+}
 
+function register() {
+    let name = document.getElementById('input_name').value;
+    let email = document.getElementById('input_email').value;
+    let senha = document.getElementById('input_password').value;
 
+    if (name == '') {
+        alert('Insira um nome para efetuar o cadastro!');
+
+    } else if (email == '') {
+        alert('Insira um email para efetuar o cadastro!');
+
+    } else if (senha.length < 8) {
+        alert('A senha deve conter 8 ou mais caracteres!');
+        
+    } else {
+        alert('Cadastro Realizado!');
+        
+        document.getElementById('input_name').value = '';
+        document.getElementById('input_email').value = '';
+        document.getElementById('input_password').value = '';
+    }
 }
